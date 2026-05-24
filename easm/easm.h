b0a10b62349f71d2570ef8be2bb8181f34e74746
如风 <3262150477@qq.com>
2026-05-25 01:20:47 +0800

    feat(project): 重构基础接口并添加 CodeBuffer 骨架

--- easm/easm.h
+++
@@ -1,5 +0,0 @@
-﻿#pragma once
-
-#include <iostream>
-
-// TODO: 在此处引用程序需要的其他标头。
--- easm/internal/memory/virtmem.h
+++
@@ -1,9 +0,0 @@
-#ifndef EASM_VIRTMEM_H
-#define EASM_VIRTMEM_H
-
-namespace Easm {
-    class VirtMem {
-    };
-} // Easm
-
-#endif //EASM_VIRTMEM_H
